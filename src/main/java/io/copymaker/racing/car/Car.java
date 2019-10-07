13f721ae5bcad936c96ce484527d9889e20fd662
copymaker <duoback0@gmail.com>
2019-10-07 22:01:31 +0900

    Car 클래스 및 테스트
    Car 는 5자 이하의 이름을 가져야 한다.
    Car 는 입력된 값만큼 이동해야 한다.

--- src/main/java/io/copymaker/racing/car/Car.java
+++ src/main/java/io/copymaker/racing/car/Car.java
@@ -1,4 +1,37 @@
 package io.copymaker.racing.car;
 
 public class Car {
+
+    private String name;
+    private int distance;
+
+    public Car(String name) {
+        this(name, 0);
+    }
+
+    /**
+     * TODO: 자동차의 최대 이동거리는 트랙에 의해 결정되는데, 자동차의 생성시점에 distance 의 경계값을 체크할 방법은?
+     */
+    public Car(String name, int distance) {
+        if (name == null || name.equals("") || name.length() > 5) {
+            throw new IllegalArgumentException("자동차의 이름은 5자 이내의 문자열이어야 합니다.");
+        }
+        this.name = name;
+        this.distance = distance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void forward(int distance) {
+        if (distance < 0 || distance > 100) {
+            throw new IllegalArgumentException("자동차의 이동거리는 0~100 사이여야 합니다.");
+        }
+        this.distance += distance;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
 }
--- src/test/java/io/copymaker/racing/car/CarTest.java
+++ src/test/java/io/copymaker/racing/car/CarTest.java
@@ -1,8 +1,77 @@
 package io.copymaker.racing.car;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.assertj.core.api.Assertions.*;
 
 class CarTest {
+
+    private Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car("TEST");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"A", "AB", "ABC", "ABCD", "ABCDE"})
+    @DisplayName("자동차는 이름을 가져야 한다.")
+    void shouldCarHasName(String name) {
+        car = new Car(name);
+
+        assertThat(car.getName()).isEqualTo(name);
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {"", "ABCDEFG", "ABCDEFGHIJ"})
+    @DisplayName("자동차 이름은 NULL, 공백 제외 5자 이하여야 한다.")
+    void shouldThrownExceptionWhenCarNameLengthOver5(String name) {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            car = new Car(name);
+        });
+    }
+
+    @Test
+    @DisplayName("자동차의 초기 이동 거리는 0이다.")
+    void shouldReturnZeroWhenCreatedCarsDistance() {
+        assertThat(car.getDistance()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("자동차는 주어진 숫자만큼 이동해야 한다.")
+    void shouldCarMoveWhenForwardWithIntegerValue() {
+        car.forward(1);
+        assertThat(car.getDistance()).isEqualTo(1);
+
+        car.forward(2);
+        assertThat(car.getDistance()).isEqualTo(3);
+
+        car.forward(3);
+        assertThat(car.getDistance()).isEqualTo(6);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {200, Integer.MAX_VALUE})
+    @DisplayName("자동차 이동거리는 0~100 사이의 값이어야 한다.")
+    void shouldThrownExceptionWhenForwardWithHugeValue(int distance) {
+        assertThrows(IllegalArgumentException.class, () -> {
+            car.forward(distance);
+        });
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("자동차의 최대 이동거리는 트랙의 길이와 같다.")
+    void shouldWriteTest() {
+
+    }
 
 }
