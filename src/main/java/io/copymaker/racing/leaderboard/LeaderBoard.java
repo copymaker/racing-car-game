ef5aff38d7ab99b7a7e9ff728374a602e5c297fa
copymaker <duoback0@gmail.com>
2019-10-08 20:09:35 +0900

    LeaderBoard 클래스 레이싱 기록 및 현황 출력

---
+++ src/main/java/io/copymaker/racing/leaderboard/LeaderBoard.java
@@ -0,0 +1,12 @@
+package io.copymaker.racing.leaderboard;
+
+import io.copymaker.racing.car.Car;
+
+import java.util.List;
+
+public class LeaderBoard {
+
+    public List<Car> findWinningCars() {
+        return null;
+    }
+}
